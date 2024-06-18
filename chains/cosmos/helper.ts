72574789bb81899058f2314d5a52d5788f06b8fd
nahyunbak <kirina@composable.finance>
2024-06-18 20:16:17 +0900

    feat: add cosmos sdk

---
+++ chains/cosmos/helper.ts
@@ -0,0 +1,66 @@
+import {
+  SigningStargateClient,
+ 
+} from '@cosmjs/stargate';
+import {TX_MSG_TYPE} from './types'
+import { TIMEOUT_IBC_MAX } from './constants';
+
+
+
+export const generateTransferMsg = (
+	txMsg: TX_MSG_TYPE,
+	channel: number,
+	sourceAddress: string,
+  destAddress: string,
+	amount: string,
+	assetId: string,
+	memo: string,
+	timeout: number = 30
+) => {
+	const msg = {
+		typeUrl: txMsg, 
+		value: {
+			sourcePort: 'transfer',
+			sourceChannel: `channel-${channel}`,
+			token: {
+				denom: assetId,
+				amount: amount
+			},
+			sender: sourceAddress,
+			receiver: destAddress,
+			memo,
+			timeoutTimestamp: getCosmosTimeoutTimestamp(timeout * 60) //  30~240 minutes
+		}
+	};
+	return msg;
+};
+
+/** description the amount of seconds for timeout */
+export const getCosmosTimeoutTimestamp = (seconds: number) => {
+	return new Date().getTime() * 1_000_000 + seconds * 1_000 * 1_000_000;
+};
+
+/** @description chainId is from cosmos chain registry */
+export const getSigner = (chainId:string)=> {
+	return keplr?.getOfflineSigner(chainId)
+}
+
+export const keplr = window.keplr; // provider of cosmos wallet 
+
+export const getClient = async (chainId:string, rpc:string)=>{
+	const signer = getSigner(chainId);
+	return await SigningStargateClient.connectWithSigner(rpc, signer);
+}
+
+
+export const memoBuilder = ({ destChannel, destAddress }: { destChannel: number; destAddress: string }) => {
+	return JSON.stringify({
+		forward: {
+			receiver: destAddress,
+			port: 'transfer',
+			channel: `channel-${destChannel}`,
+			timeout: TIMEOUT_IBC_MAX,
+			retries: 0
+		}
+	});
+};
