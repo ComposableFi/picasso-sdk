341e909b57fdb5109f2504eb5e806ceabb61fefe
nahyunbak <kirina@composable.finance>
2024-07-17 04:23:59 +0900

    fix: update version with export

--- dist/chains/solana/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './methods';
-export * from './constants';
-export * from './helper';
-//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './chains';
 export * from './config';
+export * from './indexer';
