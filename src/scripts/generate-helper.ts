e4a41233c4d4ee181d57cfbf3c83a39e4b89b83a
nahyunbak <kirina@composable.finance>
2024-08-03 05:38:55 +0900

    fix: update path

--- src/scripts/generate-helper.ts
+++ src/scripts/generate-helper.ts
@@ -1,97 +1,33 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { CrosschainAsset } from '../src/config/types';
+import {
+  CoinGeckoAsset,
+  CrosschainAsset,
+  CrossChainAssets,
+  EthereumAsset,
+  NetworkInfo,
+  SolanaAsset,
+  TokenPerChannel,
+} from '../config/types';
 
-interface EthereumAsset {
-  erc20Address: string;
-  cosmosToEthereumFee: number;
-  minimumTransfer: number;
-}
-
-interface SolanaAsset {
-  mintAddress: string;
-  minimumTransfer: number;
-}
-interface TokenPerChannel {
-  chainId: string;
-  destinationId: number;
-  tokens: string[];
-}
-interface CoinGeckoAsset {
-  name: string;
-  id: string;
-}
-
-interface CrossChainAssets {
-  cosmos: Record<string, CrosschainAsset>;
-  solana: Record<string, CrosschainAsset>;
-  ethereum: Record<string, CrosschainAsset>;
-  dotsama: Record<string, CrosschainAsset>;
-}
-
-interface NetworkInfo {
-  name: string;
-  image: string;
-  rpc: string;
-  rest?: string;
-  chain_type: 'polkadot' | 'solana' | 'ethereum';
-  chainId: string;
-  feeAssetId: string;
-  polkadot?: {
-    ss58Format: number;
-    isParachain: boolean;
-    relayChain: 'polkadot' | 'kusama';
-    hops: {
-      [key: string]: {
-        type: 'xcm';
-        xcmType: string;
-        version: string;
-        tokens: string[];
-      };
-    };
-  };
-  cosmos?: {
-    bip44: { coinType: number };
-    bech32Config: {
-      bech32PrefixAccAddr: string;
-      bech32PrefixAccPub: string;
-      bech32PrefixValAddr: string;
-      bech32PrefixValPub: string;
-      bech32PrefixConsAddr: string;
-      bech32PrefixConsPub: string;
-    };
-  };
-  enabled: boolean;
-  network_to?: string[];
-}
-
-const dataDir = path.join(__dirname, '../src/config/json');
+const dataDir = path.join(__dirname, '../config/json');
 const ethereumOutputFilePath = path.join(
   __dirname,
-  '../src/config/ethereumAssets.ts'
+  '../config/ethereumAssets.ts'
 );
-const solanaOutputFilePath = path.join(
-  __dirname,
-  '../src/config/solanaAssets.ts'
-);
+const solanaOutputFilePath = path.join(__dirname, '../config/solanaAssets.ts');
 
 const tokensPerChannelOutputFilePath = path.join(
   __dirname,
-  '../src/config/tokensPerChannel.ts'
+  '../config/tokensPerChannel.ts'
 );
-const coinGeckoOutputFilePath = path.join(
-  __dirname,
-  '../src/config/coinGecko.ts'
-);
+const coinGeckoOutputFilePath = path.join(__dirname, '../config/coinGecko.ts');
 const crossChainAssetsOutputFilePath = path.join(
   __dirname,
-  '../src/config/crossChainAssets.ts'
+  '../config/crossChainAssets.ts'
 );
 
-const networksOutputFilePath = path.join(
-  __dirname,
-  '../src/config/networks.ts'
-);
+const networksOutputFilePath = path.join(__dirname, '../config/networks.ts');
 
 const processFiles = () => {
   const ethereumAssets: Record<string, EthereumAsset> = {};
