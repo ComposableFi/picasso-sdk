d3dac61dd29b6d4d2c1a3d15bfb4983cf51c5c90
nahyunbak <kirina@composable.finance>
2024-08-19 17:52:42 +0900

    fix: deploy new npm

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ export * from './ethereumAssets';
 export * from './solanaAssets';
 export * from './tokensPerChannel';
 export * from './networks';
-export * from './types.d';
+export * from './types';
 export * from './keplrChains';
