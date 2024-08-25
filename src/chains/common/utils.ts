0649cbefb6f2d30a271964a0c54b7386a4e7fc91
nahyunbak <kirina@composable.finance>
2024-08-26 02:10:57 +0900

    feat: add util functions

--- src/chains/common/utils.ts
+++ src/chains/common/utils.ts
@@ -1,8 +1,14 @@
 import EventEmitter from 'eventemitter3';
 import { WalletApiEvents } from './types';
 import Big from 'big.js';
-import { networks, tokensPerChannel, TransferType } from '../../config';
-
+import {
+  keplrChains,
+  networks,
+  tokensPerChannel,
+  TransferType,
+} from '../../config';
+import { fromBech32, fromHex, toBech32 } from '@cosmjs/encoding';
+import { decodeAddress, encodeAddress } from '@polkadot/util-crypto';
 export const emitter = new EventEmitter<WalletApiEvents>();
 export const TIMEOUT_IBC_MAX = 6000000000000;
 
@@ -121,4 +127,62 @@ export const getSupportedType = (
     return 'multihop';
   }
   return;
+};
+
+export const getPolkadotAddressStr = (
+  accountId: string,
+  prefix?: number
+): string => {
+  try {
+    return encodeAddress(accountId, prefix || 0);
+  } catch (e) {
+    return accountId;
+  }
+};
+
+export const convertCosmosAddress = (
+  address: string,
+  newPrefix: string
+): string => {
+  try {
+    // Bech32 주소를 디코딩하여 5바이트 접두사 제거
+    const { data } = fromBech32(address);
+    // 새로운 접두사로 다시 Bech32 주소 생성
+
+    return toBech32(newPrefix, data);
+  } catch (e) {
+    throw new Error('유효하지 않은 Bech32 주소입니다.');
+  }
+};
+
+/**@description When it comes to Cosmos network, coinType should be 114 to use this converter*/
+export const convertAddressToStr = (
+  address: string,
+  fromChainId: string
+): string => {
+  if (address.startsWith('0x')) {
+    // 2004: moonbeam, 2023:moonriver
+    if (['ethereum', 'solana', '2004', '2023'].some((v) => v === fromChainId)) {
+      return address;
+    } else if (
+      networks[origin].chainType === 'polkadot' &&
+      networks[origin].polkadot?.ss58Format
+    ) {
+      return getPolkadotAddressStr(
+        address,
+        Number(networks[origin].polkadot.ss58Format)
+      );
+    } else if (
+      networks[origin].chainType === 'cosmos' &&
+      networks[origin].cosmos.bech32Config.bech32PrefixAccAddr
+    ) {
+      return convertCosmosAddress(
+        address,
+        networks[origin].cosmos.bech32Config.bech32PrefixAccAddr
+      );
+    } else {
+      return address;
+    }
+  }
+  return address;
 };
