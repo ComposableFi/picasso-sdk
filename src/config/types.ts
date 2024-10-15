dec6d387a1d420d8b654e2ddb256aad2f7cce374
nahyunbak <kirina@composable.finance>
2024-10-15 22:32:06 +0900

    feat: generate with new field at crossChainAssets

--- src/config/types.ts
+++ src/config/types.ts
@@ -5,12 +5,12 @@ export { type ChainInfo } from '@keplr-wallet/types';
 export interface CrosschainAsset {
   chainId: string;
   decimals: number;
-  network?: string;
   minimalDenom: string;
   denom: string;
   imageUrl?: string;
   realDecimals?: number; // only used at solana, because it is restricted not to use more than 9
   ratio?: { n: string; d: string };
+  currentChainId?:string; // current network. 
 }
 
 export interface EthereumAsset {
--- src/scripts/generate-helper.ts
+++ src/scripts/generate-helper.ts
@@ -158,6 +158,7 @@ const processFiles = () => {
             minimalDenom: currency.ethereum.minimalDenom || '',
             denom: coinDenom || '',
             imageUrl: currency?.coinImageUrl || '',
+            currentChainId: 'ethereum',
           };
         }
       }
@@ -182,6 +183,8 @@ const processFiles = () => {
             denom: coinDenom || '',
             realDecimals,
             imageUrl: currency.coinImageUrl || '',
+            currentChainId: 'ethereum',
+
           };
         }
       }
@@ -196,6 +199,7 @@ const processFiles = () => {
             denom: coinDenom,
             imageUrl: currency.coinImageUrl,
             ratio: currency.polkadot.ratio,
+            currentChainId: picassoAssetId,
           };
         }
         if (currency.polkadot?.composableAssetId) {
@@ -206,17 +210,21 @@ const processFiles = () => {
             denom: coinDenom,
             imageUrl: currency.coinImageUrl,
             ratio: currency.polkadot.ratio,
+            currentChainId: composableAssetId,
+
           };
         }
       }
       // generate cosmosAssets.ts
-      if (currency.cosmos) {
+      if (currency.cosmos && currency.cosmos.minimalDenom) {
         crossChainAssets.cosmos[currency.cosmos.minimalDenom] = {
           chainId: data.chainId || '',
           decimals: currency.coinDecimals || '',
           minimalDenom: currency.cosmos.minimalDenom || '',
           denom: coinDenom || '',
           imageUrl: currency.coinImageUrl || '',
+          currentChainId: data.chainId || '',
+
         };
       }
 
